dcae48ffcf71fb98bbc594bf5fcdc0666eb5e156
AB0529 <supercrab115@gmai.com>
2021-02-06 09:08:00 -0500

    Removed screenshot, and updated some things

---
+++ Scripts/Misc/recorder.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+case "$1" in 
+    gif)
+        TMPFILE="$(mktemp -t screencast-XXXXXXX).mkv"
+        OUTPUT=/tmp/out
+
+        read -r X Y W H G ID < <(slop -f "%x %y %w %h %g %i")
+        ffmpeg -f x11grab -s "$W"x"$H" -i :0.0+$X,$Y "$TMPFILE"
+
+        notify-send -t 3 -i $PICS/Icons/camera-ico.png 'Generating GIF'
+        ffmpeg -y -i "$TMPFILE"  -vf fps=10,palettegen /tmp/palette.png
+        ffmpeg -y -i "$TMPFILE" -i /tmp/palette.png -filter_complex "paletteuse" $OUTPUT.gif
+        rm -f $TMPFILE
+
+        dragon-drag-and-drop --and-exit $OUTPUT.gif
+
+        trap "rm -f '$TMPFILE'" 0
+    ;;
+    vid)
+    ;;
+esac
