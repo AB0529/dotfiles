005d9c8b1134dbe99534e90b40c30f36d8ec143d
AB0529 <supercrab115@gmail.com>
2021-03-10 14:54:24 -0500

    Update stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,3 @@
-#!/bin/fish
 # Load env vars
 source $XDG_CONFIG_HOME/fish/env.fish
 
@@ -9,7 +8,6 @@ neofetch
 # Load aliases file
 [ -f "$XDG_CONFIG_HOME/fish/aliases" ] && source $XDG_CONFIG_HOME/fish/aliases
 source $XDG_CONFIG_HOME/fish/git_aliases
-
 
 # Starship prompt
 starship init fish | source
