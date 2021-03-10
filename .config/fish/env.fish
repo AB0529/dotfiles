bd92c04ac4cef1a7e42d328e480dfe8d592def05
AB0529 <supercrab115@gmail.com>
2021-03-10 14:57:37 -0500

    Moved universal vars into fish_variables

--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -5,11 +5,6 @@ set TERMINAL /usr/bin/alacritty
 set BROWSER /usr/bin/librewolf
 set EDITOR /usr/bin/nvim
 
- set XDG_CONFIG_HOME "$HOME/.config"
- set XDG_CACHE_HOME "$HOME/.cache"
- set XDG_DATA_HOME "$HOME/.local/share"
- set XDG_DESKTOP_DIR "$HOME"
- set XINITRC "$XDG_CONFIG_HOME/X11/xinitrc"
  set CODING $HOME/Coding
  set PICS $HOME/Pics
 
