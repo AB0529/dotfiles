97ccf630ee3420851feb8aa3fbc8bcf1517f42b2
AB0529 <supercrab115@gmail.com>
2021-03-10 15:07:55 -0500

    Fixed stuff

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,5 @@
 # Load env vars
-source $XDG_CONFIG_HOME/fish/env.fish
+source $HOME/.config/fish/env.fish
 
 # Vanity
 cat ~/.cache/wal/sequences
--- .config/fish/env.fish
+++ .config/fish/env.fish
@@ -1,9 +1,15 @@
 #!/bin/fish
 set -e fish_greeting
 
-set TERMINAL /usr/bin/alacritty
-set BROWSER /usr/bin/librewolf
-set EDITOR /usr/bin/nvim
+set -g TERMINAL /usr/bin/alacritty
+set -g BROWSER /usr/bin/librewolf
+set -g EDITOR /usr/bin/nvim
+
+set -g XDG_CACHE_HOME "$HOME/.cache"
+set -g XDG_CONFIG_HOME "$HOME/.config"
+set -g XDG_DATA_HOME "$HOME/.local/share"
+set -g XDG_DESKTOP_DIR "$HOME"
+set -g XINITRC "$XDG_CONFIG_HOME/X11/xinitrc"
 
  set CODING $HOME/Coding
  set PICS $HOME/Pics
