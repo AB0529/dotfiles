e56eda51876a2c10ab790c85a7a2cc6de0fb072c
AB0529 <supercrab115@gmail.com>
2021-07-13 12:58:42 +0000

    Updated keybinds

--- Scripts/Audio/audio.sh
+++ Scripts/Audio/audio.sh
@@ -11,6 +11,13 @@ case "$1" in
     mute) [ $mute = true ] && pactl set-sink-mute $sink "no" || pactl set-sink-mute $sink "yes" ; pkill -RTMIN+15 dwmblocks ;;
     volup) pactl set-sink-volume $sink +$inc% ;;
     voldown) pactl set-sink-volume $sink -$inc% ;;
+    volset) 
+        v=$(rofi -dmenu -p "🔊 Set Volume" -lines 0 -separator-style "none" -theme ab-theme.rasi &)
+        wait
+        echo $v
+        [ $v -gt 100 ] && v=100
+        [ $v -lt 0 ] && v=0
+        pactl set-sink-volume $sink $v% ;;
 esac
 
 [ $mute = true ] && echo "🔇 $vol%" && exit
