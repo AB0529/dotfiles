e750a3b6a405cbd728b10b26f6553a185cab258b
AB0529 <supercrab115@gmail.com>
2021-10-12 16:05:06 -0400

    Moved autostart to bsprc

--- Scripts/Misc/changetheme.sh
+++ Scripts/Misc/changetheme.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-/bin/ls $HOME/.config/bspwm/themes | rofi -dmenu -theme ~/.config/rofi/styles.rasi | xargs -i fish -c 'xrdb $HOME/.config/bspwm/themes/{} | cat $HOME/.config/bspwm/themes/{} > $HOME/.config/X11/Xresources' && $HOME/Scripts/Misc/rofi_gen.sh > $HOME/.config/rofi/colors.rasi 
+if [ -z "$1" ]; then 
+    /bin/ls $HOME/.config/bspwm/themes | rofi -dmenu -theme ~/.config/rofi/styles.rasi | xargs -i fish -c 'xrdb $HOME/.config/bspwm/themes/{} | cat $HOME/.config/bspwm/themes/{} > $HOME/.config/X11/Xresources' && $HOME/Scripts/Misc/rofi_gen.sh > $HOME/.config/rofi/colors.rasi 
+else
+    xrdb $HOME/.config/bspwm/themes/$1 | cat $HOME/.config/bspwm/themes/$1 > $HOME/.config/X11/Xresources && $HOME/Scripts/Misc/rofi_gen.sh > $HOME/.config/rofi/colors.rasi && $HOME/Scripts/System/polybar.sh
+fi
