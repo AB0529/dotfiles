0fa322ea702a4555b702c6ef6f7d759b4df0d208
AB0529 <supercrab115@gmail.com>
2021-10-24 23:39:59 -0400

    Update aliases

--- Scripts/Texts/fuckyou.sh
+++ Scripts/Texts/fuckyou.sh
@@ -3,6 +3,6 @@
 for i in {1..9..2} 
 do
     echo $i
-    awk -vi=$i 'FNR == i {print}' $HOME/Scripts/Texts/fuckyou.txt | xsel --clipboard
+    awk -vi=$i 'FNR == i {print}' $HOME/Scripts/Texts/fuckyou.txt | xclip -sel clip
     read -p "Press key to continue.. " -n1 -s
 done
