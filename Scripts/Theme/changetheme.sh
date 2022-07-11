ab8a6e94952fb22868c8c98dab0820aafd98d180
AB0529 <supercrab115@gmail.com>
2022-07-11 00:47:11 -0400

    Fixed rofi colors not changing

--- Scripts/Theme/changetheme.sh
+++ Scripts/Theme/changetheme.sh
@@ -7,10 +7,10 @@ change_theme() {
 
     # Add the colors to XResources
     cp $XDG_CONFIG_HOME/bspwm/themes/$THEME $XDG_CONFIG_HOME/X11/Xresources
-    # Run rofi_gen script to change rofi colors
-    $SCRIPTS/Theme/rofi_gen.sh > $XDG_CONFIG_HOME/rofi/colors.rasi
     # Refresh colors
     xrdb $XDG_CONFIG_HOME/X11/Xresources
+    # Run rofi_gen script to change rofi colors
+    $SCRIPTS/Theme/rofi_gen.sh > $XDG_CONFIG_HOME/rofi/colors.rasi
     # Restart polybar
     $SCRIPTS/System/polybar.sh &>/dev/null
 
