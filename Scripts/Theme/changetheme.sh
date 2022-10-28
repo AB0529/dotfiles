1cf6a8c26cf194e1013de4a5b97fe050e380de1d
AB0529 <supercrab115@gmail.com>
2022-10-27 20:44:25 -0400

    Updated rofi colors, themes, and theme changing script

--- Scripts/Theme/changetheme.sh
+++ Scripts/Theme/changetheme.sh
@@ -1,26 +1,32 @@
 #!/bin/bash
 
-ROFI_THEME=$XDG_CONFIG_HOME/rofi/styles.rasi
+# TODO: Clean up script
+
+ROFI_THEME=$XDG_CONFIG_HOME/rofi/colors.rasi
 
 change_theme() {
-    COLORS=$(cat $XDG_CONFIG_HOME/bspwm/themes/$1)
-
     # Add the colors to XResources
-    cp $XDG_CONFIG_HOME/bspwm/themes/$THEME $XDG_CONFIG_HOME/X11/Xresources
+    # cp $XDG_CONFIG_HOME/rofi/colors/$THEME $XDG_CONFIG_HOME/X11/Xresources
+
+    cat $XDG_CONFIG_HOME/rofi/colors/$1 | sed -e "1,8d;9d;16d" -e 's/;\+$//' -e 's/..$//' -e 's/^/*/' -e 's/\\s\\+//g' > $XDG_CONFIG_HOME/X11/Xresources
     # Refresh colors
     xrdb $XDG_CONFIG_HOME/X11/Xresources
-    # Run rofi_gen script to change rofi colors
-    $SCRIPTS/Theme/rofi_gen.sh > $XDG_CONFIG_HOME/rofi/colors.rasi
+
+    # Remove old colors.rasi
+    rm $ROFI_THEME
+
+    # Create new one with new theme
+    printf "@import \"~/.config/rofi/colors/$1\"" > $ROFI_THEME
+
     # Restart polybar
     $SCRIPTS/System/polybar.sh &>/dev/null
-
 }
 
 # Theme argument is not provided
 if [[ -z "$1" ]]
 then
     # Get the theme name and the file contents
-    THEME=$(/bin/ls $XDG_CONFIG_HOME/bspwm/themes | rofi -dmenu -theme $ROFI_THEME)
+    THEME=$(/bin/ls $XDG_CONFIG_HOME/rofi/colors | rofi -dmenu -theme $XDG_CONFIG_HOME/rofi/message.rasi)
     change_theme $THEME
 else
     THEME=$1
