1e5af5c70b4a2ce4cd64f4e3cebbdb3309668728
AB0529 <supercrab115@gmail.com>
2022-10-28 14:16:19 -0400

    Clean up

---
+++ Scripts/System/autostart.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Xrandr
+xrandr --output HDMI-A-0 --primary --output DisplayPort-0 --left-of HDMI-A-0 --output DisplayPort-0 --rotate right
+
+# Set cursor
+xsetroot -cursor_name left_ptr &
+
+# Hotkey Daemon
+sxhkd -c $HOME/.config/sxhkd/bspwm \
+    $HOME/.config/sxhkd/system \
+    $HOME/.config/sxhkd/apps \
+    $HOME/.config/sxhkd/audio &
+
+sh-changetheme $THEME.rasi &
+
+unclutter -idle 3 &
+xwallpaper --zoom .wallpaper.jpg
+picom &
