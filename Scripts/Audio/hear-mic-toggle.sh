01aad1d7a20ceb3161cb8a2b406330322b372a29
AB0529 <supercrab115@gmail.com>
2022-11-01 10:39:32 -0400

    Update things

--- Scripts/Audio/hear-mic-toggle.sh
+++ Scripts/Audio/hear-mic-toggle.sh
@@ -7,13 +7,13 @@ NO_HEAR_ICO=~/Pics/Icons/speaker-mute-ico.png
 hear() {
 	touch $TOGGLE
 	pactl load-module module-loopback latency_msec=1
-	notify-send -i $HEAR_ICO "Hear Microphone" "Now nearing microphone"
+	notify-send -t 3000 -i $HEAR_ICO "Hear Microphone" "Now nearing microphone"
 }
 
 stophear() {
 	rm $TOGGLE
 	pactl unload-module module-loopback
-	notify-send -i $NO_HEAR_ICO "Hear Microphone" "No longer hearing microphone"
+	notify-send -t 3000 -i $NO_HEAR_ICO "Hear Microphone" "No longer hearing microphone"
 }
 
 [ ! -e $TOGGLE ] && hear || stophear
--- Scripts/Audio/switch_speaker.sh
+++ Scripts/Audio/switch_speaker.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 icon="$HOME/Pics/Icons/speaker-ico.png"
-SPEAKERS="alsa_output.pci-0000_01_00.1.hdmi-stereo-extra3"
 
 for playing in $(pacmd list-sink-inputs | awk '$1 == "index:" {print $2}')
 do
