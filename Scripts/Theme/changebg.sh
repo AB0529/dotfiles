17a14cc254ebfa05ad94f5223e6db965a0561039
AB0529 <supercrab115@gmail.com>
2023-05-24 21:18:49 -0400

    Fixed bg stuff

--- Scripts/Theme/changebg.sh
+++ Scripts/Theme/changebg.sh
@@ -2,4 +2,4 @@
 # Open wallpapers in SXIV and get the selected output
 wallpaper=`sxiv -rtqo /home/ab/Pics/Wallpapers`
 # Set wallpaper if not empty
-[ -z "$wallpaper" ] && echo "Press 'm' to mark!" || cp $wallpaper $HOME/.wallpaper.jpg && xwallpaper --zoom $HOME/.wallpaper.jpg
+[ -z "$wallpaper" ] && echo "Press 'm' to mark!" || cp "$wallpaper" $HOME/.wallpaper.jpg && xwallpaper --zoom $HOME/.wallpaper.jpg
